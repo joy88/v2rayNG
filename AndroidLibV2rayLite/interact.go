07c9e06bf3c2ef167705db4b06bc379ca9c46304
WeiquanShen <2331887703@qq.com>
2020-05-25 16:57:33 +0800

    1. github.com/2dust/AndroidLibV2rayLite/Process/Escort -> github.com/2dust/v2rayNG/AndroidLibV2rayLite/Process/Escort

--- AndroidLibV2rayLite/interact.go
+++ AndroidLibV2rayLite/interact.go
@@ -9,9 +9,9 @@ import (
 	"sync"
 
 	"github.com/2dust/v2rayNG/AndroidLibV2rayLite/CoreI"
-	"github.com/2dust/AndroidLibV2rayLite/Process/Escort"
-	"github.com/2dust/AndroidLibV2rayLite/VPN"
-	"github.com/2dust/AndroidLibV2rayLite/shippedBinarys"
+	"github.com/2dust/v2rayNG/AndroidLibV2rayLite/Process/Escort"
+	"github.com/2dust/v2rayNG/AndroidLibV2rayLite/VPN"
+	"github.com/2dust/v2rayNG/AndroidLibV2rayLite/shippedBinarys"
 	mobasset "golang.org/x/mobile/asset"
 
 	v2core "v2ray.com/core"
